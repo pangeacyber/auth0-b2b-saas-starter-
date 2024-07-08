3a89511c9eef84b59928c118cffdfd7bc2f72afc
Michael Combs <michael.combs@pangea.cloud>
2024-07-08 10:57:06 -0700

    Put services in correct order

--- app/pangea/services/page.tsx
+++ app/pangea/services/page.tsx
@@ -36,36 +36,6 @@ export default async function PangeaDemo() {
         <hr className="mt-6" />
       </div>
       <ServiceEntry
-        name="Redact"
-        description="
-            Pangea’s Redact service helps developers control sensitive
-            information by performing redaction using defined rules. Redact
-            comes equipped with out-of-the-box rules to address personally
-            identifiable information (PII), geographic locations, payment card
-            industry (PCI) data, and many other types of sensitive information,
-            while also providing rule customization to fit the needs of your
-            application."
-        icon={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="-mt-1 inline size-8 text-purple-800"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-            />
-          </svg>
-        }
-        settingsLink="https://console.pangea.cloud/service/redact"
-        documentationLink="https://pangea.cloud/docs/redact"
-        tickleLink="https://pangea.cloud/docs/api/redact/"
-      />
-      <ServiceEntry
         name="Secure Audit Log"
         description="
           A secure audit log is a critical component of any system that requires accountability and transparency by providing an accurate and secure record of all system events.
@@ -114,6 +84,36 @@ export default async function PangeaDemo() {
         settingsLink="https://console.pangea.cloud/service/share"
         documentationLink="https://pangea.cloud/docs/share"
         tickleLink="https://pangea.cloud/docs/api/share/"
+      />
+      <ServiceEntry
+        name="Redact"
+        description="
+            Pangea’s Redact service helps developers control sensitive
+            information by performing redaction using defined rules. Redact
+            comes equipped with out-of-the-box rules to address personally
+            identifiable information (PII), geographic locations, payment card
+            industry (PCI) data, and many other types of sensitive information,
+            while also providing rule customization to fit the needs of your
+            application."
+        icon={
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="-mt-1 inline size-8 text-purple-800"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+            />
+          </svg>
+        }
+        settingsLink="https://console.pangea.cloud/service/redact"
+        documentationLink="https://pangea.cloud/docs/redact"
+        tickleLink="https://pangea.cloud/docs/api/redact/"
       />
       <ServiceEntry
         name="Sanitize"
