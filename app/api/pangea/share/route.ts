2b15e55a5ee270533c2ecaeeae649d29a27682c9
Michael Combs <michael.combs@pangea.cloud>
2024-07-16 15:54:23 -0700

    Refactor share client to include a scoped client

--- app/api/pangea/share/route.ts
+++ app/api/pangea/share/route.ts
@@ -2,7 +2,6 @@ import { NextRequest } from "next/server"
 
 import { appClient } from "@/lib/auth0"
 import { SecureShareClient } from "@/lib/pangea"
-import { getShareLinks, getObject, performFetch, linkTags, tagsMatch } from "./utils"
 
 const shareClient = new SecureShareClient()
 
@@ -27,238 +26,40 @@ async function fetchStore(
   action: string,
   body: any
 ): Promise<Response> {
-  let auditAction: null | string = null
-  let auditActor: null | string = null
-  let auditMessage: null | string = null
+  const scopedShareClient = shareClient.scopedClient(orgId, email)
 
-  const rootFolder = `/${orgId}/${email}`
   switch (action) {
-    case "/v1beta/get": {
-      const objectResp = await getObject(email, orgId, body.id, body.path)
-      if (objectResp instanceof Response) {
-        return objectResp as Response
-      }
+    case "/v1beta/get":
+      return scopedShareClient.getObject(body)
 
-      const folder = objectResp.result.object.folder
-      objectResp.result.object.folder = (folder as string).replace(
-        `/${orgId}/${email}`,
-        ""
-      )
-      if (body.transfer_method) {
-        auditAction = "Downloaded File"
-        auditActor = email
-        auditMessage = `${email} downloaded ${body.id || body.path}`
-      } else {
-        return new Response(JSON.stringify(objectResp), {
-          status: 200,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-      }
-    }
-    break;
-    case "/v1beta/list": {
-      // If a filter is not present, or the filter folder does not start with email
-      if (body.filter && body.filter.parent_id) {
-        const parent = await getObject(email, orgId, body.filter.parent_id)
-        if (parent instanceof Response) {
-          return parent
-        }
-      }
+    case "/v1beta/list":
+      return scopedShareClient.listObjects(body)
 
-      if (body.filter && body.filter.id) {
-        const folder = await getObject(email, orgId, body.filter.id)
-        if (folder instanceof Response) {
-          return folder
-        }
-      }
-
-      if (
-        body.filter &&
-        body.filter.folder &&
-        !(body.filter.folder as string).startsWith(rootFolder)
-      ) {
-        body.filter.folder = rootFolder + body.filter.folder
-      }
-
-      if (
-        !body.filter ||
-        (!body.filter.id && !body.filter.parent_id && !body.filter.folder)
-      ) {
-        body.filter = {}
-        body.filter.folder = rootFolder
-      }
-
-      const url = `https://share.${process.env.PANGEA_DOMAIN}${action}`
-      const resp = await performFetch(url, body)
-      if (resp.status !== 200) {
-        return resp
-      }
-      const data: any = await resp.json()
-      data.result.objects = data.result.objects.map((o: any) => {
-        o.folder = (o.folder as string).replace(`/${orgId}/${email}`, "")
-        return o
-      })
-      return new Response(JSON.stringify(data), {
-        status: resp.status,
-      })
-    }
     case "/v1beta/delete":
-      {
-        const resp = await getObject(email, orgId, body.id, body.path)
-        if (resp instanceof Response) {
-          return resp as Response
-        }
-        auditAction =
-          resp.result.object.type === "folder"
-            ? "Folder Deleted"
-            : "File Deleted"
-        auditActor = email
-        auditMessage = `${email} deleted ${body.id || body.path}`
-      }
-      break
+      return scopedShareClient.deleteObject(body)
+
     case "/v1beta/update":
-      {
-        const resp = await getObject(email, orgId, body.id, body.path)
-        if (resp instanceof Response) {
-          return resp as Response
-        }
-        auditAction =
-          resp.result.object.type === "folder"
-            ? "Folder Updated"
-            : "File Updated"
-        auditActor = email
-        auditMessage = `${email} updated ${body.id || body.path}`
-      }
-      break
+      return scopedShareClient.updateObject(body)
+
     case "/v1beta/folder/create":
-      if (body.parent_id) {
-        const parent = await getObject(email, orgId, body.parent_id, body.path)
-        if (parent instanceof Response) {
-          return parent
-        }
-      } else {
-        body.path
-          ? (body.path = `${rootFolder}/${body.path}`)
-          : (body.path = `${rootFolder}/${body.name}`)
-      }
-      delete body.name;
-      auditAction = "Folder Created"
-      auditActor = email
-      auditMessage = `${email} created folder ${body.path}`
-      break
+      return scopedShareClient.createFolder(body)
+
     case "/v1beta/share/link/list":
-      {
-        const filter = body.filter || {}
-        filter.tags = linkTags(orgId, email)
-        body.filter = filter
-      }
-      break
-    case "/v1beta/share/link/get": {
-      const url = `https://share.${process.env.PANGEA_DOMAIN}/v1beta/share/link/get`
-      const resp = await performFetch(url, body)
-      if (resp.status !== 200) {
-        return resp
-      }
+      return scopedShareClient.listLinks(body)
 
-      const data: any = await resp.json()
-      if ((data.status as string).toLowerCase() !== "success") {
-        return resp
-      }
+    case "/v1beta/share/link/get":
+      return scopedShareClient.getLink(body)
 
-      const tags = data.tags as string[]
-      if (tagsMatch(orgId, email, tags)) {
-        data.result = null
-        data.status = "NotFound"
-        return new Response(JSON.stringify(data), {
-          status: 404,
-        })
-      }
-
-      return new Response(data, {
-        status: 200,
-      })
-    }
     case "/v1beta/share/link/delete":
-      {
-        const linksResp = await getShareLinks(orgId, email, body.ids)
-        const links = linksResp.result.share_link_objects.reduce(
-          (acc, o) => acc.add(o.id),
-          new Set()
-        )
-        // Make sure delete ids belong to this owner
-        for (let id of body.ids) {
-          if (!links.has(id)) {
-            return new Response(JSON.stringify({ status: "NotFound" }), {
-              status: 404,
-            })
-          }
-        }
-        auditAction = "Share Link Deleted"
-        auditActor = email
-        auditMessage = `${email} deleted share link ${body.ids[0]}`
-      }
-      break
-    case "/v1beta/share/link/create":
-      {
-        for (let link of body.links) {
-          link.tags = linkTags(orgId, email)
-          link.max_access_count = 7
-          const targets = await Promise.all(
-            link.targets.map((t: any) => getObject(email, orgId, t))
-          )
-          // Targets that don't exist should be included
-          for (let target of targets) {
-            if (target instanceof Response) {
-              return target
-            }
-          }
-          auditAction = "Share Link Created"
-          auditActor = email
-          auditMessage = `${email} created a share link`
-        }
-      }
-      break
-    case "/v1beta/share/link/send":
-      {
-        body.sender_email = email
-        const referencedLinks = body.links.map((l: any) => l.id)
-        const referencedEmails = body.links.map((l: any) => l.email)
+      return scopedShareClient.deleteLink(body)
 
-        const linksResp = await getShareLinks(orgId, email, referencedLinks)
-        const links = linksResp.result.share_link_objects.reduce(
-          (acc, o) => acc.add(o.id),
-          new Set()
-        )
-        // Make sure delete ids belong to this owner
-        for (let { id } of body.links) {
-          if (!links.has(id)) {
-            return new Response(JSON.stringify({ status: "NotFound" }), {
-              status: 404,
-            })
-          }
-        }
-        auditAction = "Share Link Sent"
-        auditActor = referencedEmails[0]
-        auditMessage = `${email} sent a share link to ${referencedEmails[0]}`
-      }
-      break
+    case "/v1beta/share/link/create":
+      return scopedShareClient.createLink(body)
+
+    case "/v1beta/share/link/send":
+      return scopedShareClient.sendLink(body)
+
     default:
       throw new Error(`Action ${action} is not implemented or does not exist`)
   }
-  const url = `https://share.${process.env.PANGEA_DOMAIN}${action}`
-  const resp = await performFetch(url, body)
-  if (
-    !auditAction ||
-    !auditActor ||
-    !auditMessage ||
-    resp.status < 200 ||
-    resp.status > 299
-  ) {
-    return resp
-  }
-
-  // await log(auditAction, auditActor, email, auditMessage)
-  return resp
 }
--- lib/pangea.ts
+++ lib/pangea.ts
@@ -1,3 +1,5 @@
+import type { ObjectStore } from "@pangeacyber/react-mui-store-file-viewer"
+
 export class PangeaClient {
   resolve202s: boolean
   retries: number
@@ -19,7 +21,7 @@ export class PangeaClient {
     this.exponentialBackoff = exponentialBackoff * 1000
   }
 
-  async request(path: string, body: any) {
+  async request(path: string, body: any): Promise<Response> {
     if (!path.startsWith("/")) {
       path = "/" + path
     }
@@ -59,7 +61,6 @@ export class PangeaClient {
 
   makeUrl(path: string): string {
     const url = `${this.proto}://${this.service}.${process.env.PANGEA_DOMAIN}${path}`
-    console.log(url)
     return url
   }
 }
@@ -103,6 +104,9 @@ export class AuditClient extends PangeaClient {
 }
 
 export class SecureShareClient extends PangeaClient {
+  orgId?: string
+  email?: string
+
   constructor(
     resolveIssue: boolean = true,
     retries: number = 5,
@@ -112,15 +116,463 @@ export class SecureShareClient extends PangeaClient {
     super("share", resolveIssue, retries, exponentialBackoff, protocol)
   }
 
-  async upload(uploadForm: FormData) {
-    return this.request("/v1beta/put", uploadForm)
+  scopedClient(orgId: string, email?: string) {
+    const clone = Object.create(Object.getPrototypeOf(this))
+    Object.assign(clone, this);
+    clone.orgId = orgId
+    clone.email = email
+    return clone
+  }
+
+  async getObject(request: ObjectStore.GetRequest): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/get", request)
+    }
+
+    const obj = await this.getObj(request.id, request.path)
+    if (obj instanceof Response) {
+      return obj
+    }
+
+    // Could be a download stream
+    return this.request("/v1beta/get", request)
+  }
+
+  async getObj(
+    id?: string,
+    path?: string
+  ): Promise<PangeaResponse<GetResponse> | Response> {
+    const resp = await this.request("/v1beta/get", { id, path })
+    if (resp.status !== 200) {
+      return resp
+    }
+
+    const data: PangeaResponse<GetResponse> = await resp.json()
+    if (data.status.toLowerCase() !== "success") {
+      return resp
+    }
+
+    if (!this.isScoped()) {
+      return resp
+    }
+
+    // Check if object is the root object
+    if (
+      data.result.object.name == this.rootPathName() &&
+      data.result.object.folder === this.scopedPath(true)
+    ) {
+      return data
+    }
+
+    // Ensure object resides within the scoped path
+    const scopedPath = this.scopedPath()
+    let folder = data.result.object.folder || "/"
+    folder = folder.endsWith("/") ? folder : folder + "/"
+
+    if (!folder.startsWith(scopedPath)) {
+      const finalResult = data as unknown as PangeaResponse<null>
+      finalResult.result = null
+      finalResult.status = "NotFound"
+      finalResult.summary = "Resource was not found"
+      return new Response(JSON.stringify(finalResult), {
+        status: 404,
+      })
+    }
+    // Emulate having a root in the component
+    data.result.object.folder = data.result.object!.folder!.slice(
+      scopedPath.length - 1
+    )
+    return data
+  }
+
+  async upload(uploadForm: FormData): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/put", uploadForm)
+    }
+
+    const requestFile = uploadForm.get("request") as File
+    const data = JSON.parse(await requestFile.text())
+
+    const scopedPath = this.scopedPath()
+    if (data.path) {
+      data.path = `${scopedPath}${data.path}/${data.name}`
+    } else {
+      data.path = `${scopedPath}${data.name}`
+    }
+    delete data.name
+
+    uploadForm.set(
+      "request",
+      new Blob([JSON.stringify(data)], { type: "application/json" })
+    )
+    const resp = await this.request("/v1beta/put", uploadForm)
+    return new Response(resp.body, {
+      status: resp.status,
+      headers: {
+        "Content-Type": resp.headers.get("Content-Type") ?? "application/json",
+      },
+    })
+  }
+
+  async getShareLinks(ids: string[]): Promise<PangeaResponse<LinksResponse>> {
+    const body = {
+      filter: { id__in: ids } as any,
+    }
+
+    if (this.isScoped()) {
+      body.filter.tags = this.linkTags()
+    }
+
+    const resp = await this.request("/v1beta/share/link/list", body)
+    return resp.json()
+  }
+
+  async listObjects(request: ObjectStore.ListRequest): Promise<Response> {
+    const objs = await this.listObjs(request)
+    if(objs instanceof Response) {
+      return objs
+    }
+
+    return new Response(JSON.stringify(objs), {
+      status: 200,
+    })
+  }
+
+  async listObjs(
+    request: ObjectStore.ListRequest,
+  ): Promise<PangeaResponse<ListObjectsResponse> | Response> {
+    if (!this.isScoped()) {
+      const data = await this.request("/v1beta/list", request)
+      return data.json()
+    }
+    const scopedPath = this.scopedPath()
+
+    const filter = request.filter || {}
+
+    if (!filter.id && !filter.parent_id && !filter.folder) {
+      filter.folder = scopedPath
+    }
+
+    if (filter.folder && !filter.folder.startsWith(scopedPath) && filter.folder != scopedPath) {
+      filter.folder = scopedPath.slice(0, -1) + filter.folder
+    }
+
+    if (filter.parent_id) {
+      const parent = await this.getObj(filter.parent_id)
+      if (parent instanceof Response) {
+        return parent
+      }
+    }
+
+    if (filter.id) {
+      const folder = await this.getObj(filter.id)
+      if (folder instanceof Response) {
+        return folder
+      }
+    }
+
+    if (!request.filter) {
+      request.filter = filter
+    }
+    const resp = await this.request("/v1beta/list", request)
+    if (resp.status !== 200) {
+      return resp
+    }
+    const data = (await resp.json()) as PangeaResponse<ListObjectsResponse>
+    data.result.objects = data.result.objects.map(
+      (obj: Partial<ObjectResponse>) => {
+        obj.folder = (obj.folder as string).replace(scopedPath.slice(0, -1), "")
+        return obj
+      }
+    )
+    return data
+  }
+
+  async deleteObject(request: ObjectStore.DeleteRequest): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/delete", request)
+    }
+    const resp = await this.getObj(request.id, request.path)
+    if (resp instanceof Response) {
+      return resp
+    }
+    return this.request("/v1beta/delete", request)
+  }
+
+  async updateObject(request: ObjectStore.UpdateRequest): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/update", request)
+    }
+
+    const resp = this.getObj(request.id, request.path)
+    if (resp instanceof Response) {
+      return resp as Response
+    }
+    return this.request("/v1beta/update", request)
+  }
+
+  async createFolder(
+    request: Partial<ObjectStore.FolderCreateRequest>
+  ): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/folder/create", request)
+    }
+    request = { ...request }
+    if (request.parent_id) {
+      const parent = await this.getObj(request.parent_id, request.path)
+      if (parent instanceof Response) {
+        return parent
+      }
+    } else {
+      const scopedPath = this.scopedPath()
+      request.path
+        ? (request.path = `${scopedPath}${request.path}`)
+        : (request.path = `${scopedPath}${request.name}`)
+      delete request.name
+    }
+    return this.request("/v1beta/folder/create", request)
+  }
+
+  async getLink(request: ObjectStore.ShareGetRequest): Promise<Response> {
+    const resp = await this.request("/v1beta/share/link/get", request)
+    if (!this.isScoped()) {
+      return resp
+    }
+
+    if (resp.status != 200) {
+      return resp
+    }
+
+    const data: any = await resp.json()
+    if ((data.status as string).toLowerCase() !== "success") {
+      return resp
+    }
+    const tags = data.tags as string[]
+    if (this.hasScopedTags(tags)) {
+      data.result = null
+      data.status = "NotFound"
+      return new Response(JSON.stringify(data), {
+        status: 404,
+      })
+    }
+    return new Response(data, {
+      status: 200,
+    })
+  }
+
+  async listLinks(request: ObjectStore.ShareListRequest): Promise<Response> {
+    request = { ...request }
+    if (this.isScoped()) {
+      const filter = request.filter || {}
+      filter.tags = filter.tags
+        ? [...filter.tags, ...this.linkTags()]
+        : this.linkTags()
+    }
+    return this.request("/v1beta/share/link/list", request)
+  }
+
+  async deleteLink(request: ObjectStore.ShareDeleteRequest): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1/share/link/delete", request)
+    }
+    const linksResp = await this.getShareLinks(request.ids)
+    if (linksResp instanceof Response) {
+      return linksResp
+    }
+
+    const { share_link_objects: links } = linksResp.result
+    const foundIds = links.map(({ id }: { id: string }) => id)
+
+    for (let searchedId of request.ids) {
+      if (!foundIds.includes(searchedId)) {
+        return new Response(JSON.stringify({ status: "NotFound" }), {
+          status: 404,
+        })
+      }
+    }
+    return this.request("/v1beta/share/link/delete", request)
+  }
+
+  async createLink(request: ShareCreateRequest): Promise<Response> {
+    request = structuredClone(request)
+    if (!this.isScoped()) {
+      return this.request("/v1beta/share/link/create", request)
+    }
+
+    const objects = new Set<string>()
+    for (let link of request.links) {
+      link.tags = this.linkTags()
+      link.max_access_count = 7
+
+      for (let target of link.targets || []) {
+        if (!objects.has(target)) {
+          objects.add(target)
+        }
+      }
+    }
+
+    const targets = await this.listObjs({
+      filter: {
+        id__in: Array.from(objects),
+      },
+    })
+
+    if (targets instanceof Response) {
+      return targets
+    }
+
+    if (targets.result.objects.length != objects.size) {
+      return new Response(
+        JSON.stringify({
+          result: null,
+          status: "NotFound",
+        }),
+        {
+          status: 404,
+        }
+      )
+    }
+    return this.request("/v1beta/share/link/create", request)
+  }
+
+  async sendLink(request: ObjectStore.ShareSendRequest): Promise<Response> {
+    if (!this.isScoped()) {
+      return this.request("/v1beta/share/link/send", request)
+    }
+
+    const ids = request.links.map((link) => link.id)
+    const linksResp = await this.getShareLinks(ids)
+
+    if (linksResp instanceof Response) {
+      return linksResp
+    }
+    const { share_link_objects: links } = linksResp.result
+    if (links.length != ids.length) {
+      return new Response(
+        JSON.stringify({
+          result: null,
+          status: "NotFound",
+        }),
+        {
+          status: 404,
+        }
+      )
+    }
+    return this.request("/v1beta/share/link/send", request)
   }
 
   async proxy(path: string, body: any) {
     return this.request(path, body)
   }
+
+  isScoped(): boolean {
+    return Boolean(this.email || this.orgId)
+  }
+
+  scopedPath(root: boolean = false): string {
+    let path = "/"
+    path = this.orgId ? `/${this.orgId}/` : path
+    path = this.email ? `${path}${this.email}/` : path
+
+    if (root) {
+      return path.split("/").slice(0, -2).join("/")
+    }
+    return path
+  }
+
+  rootPathName(): string {
+    const prefixedPath = this.scopedPath()
+    return prefixedPath.split("/").slice(-2)[0]
+  }
+
+  hasScopedTags(tags: string[]): boolean {
+    if (!this.isScoped()) {
+      return true
+    }
+    return this.linkTags().every(tags.includes)
+  }
+
+  linkTags(): string[] {
+    const tags = []
+    if (this.orgId) {
+      tags.push(`org:${this.orgId}`)
+    }
+
+    if (this.email) {
+      tags.push(`owner:${this.email}`)
+    }
+    return tags
+  }
 }
 
 function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+enum ObjectType {
+  Folder = "folder",
+  File = "file",
+}
+
+interface ObjectResponse {
+  id: string
+  name: string
+  owner?: string
+  created_at: string
+  updated_at: string
+  parent_id?: string
+  folder?: string
+  path?: string
+  billable_size: number
+  size: number
+  md5_hex?: string
+  sha256_hex?: string
+  sha512_hex?: string
+  metadata?: Record<string, any>
+  tags?: string[]
+  type: string | ObjectType
+  dest_url?: string
+
+  metadata_protected?: {
+    format?: string
+    mimetype?: string
+
+    "vault-password-algorithm"?: string
+  }
+
+  // Internal from flattenning metadata_protected
+  format?: string
+  mimetype?: string
+  "vault-password-algorithm"?: string
+}
+
+interface PangeaResponse<T = any> {
+  request_id: string
+  status: string
+  summary: string
+  result: T
+}
+
+interface GetResponse {
+  dest_url?: string
+  object: ObjectResponse
+}
+
+interface LinksResponse {
+  share_link_objects: {
+    id: string
+  }[]
+}
+
+interface ListObjectsResponse {
+  objects: Partial<ObjectResponse>[]
+  last?: string
+}
+
+interface LinkWithTags extends ObjectStore.SingleShareCreateRequest {
+  tags: string[]
+}
+
+type ShareCreateRequest = Omit<ObjectStore.ShareCreateRequest, "links"> & {
+  links: LinkWithTags[]
 }
